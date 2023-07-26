4f3758a54a967fb95b02c9b16aabbac9d4ac86c2
lautarovallelopez <lautarovallelopez@gmail.com>
2023-07-26 00:01:59 -0300

    Add title prop to FirstApp

--- 01-counter-app-cra/src/FirstApp.jsx
+++ 01-counter-app-cra/src/FirstApp.jsx
@@ -6,11 +6,12 @@ const greetingObject = {
 }
 const greet = greeting => `${greeting.greeting} ${greeting.name}`;
 
-const FirstApp = () => {
+const FirstApp = ({ title = 'Hola, soy un mensaje por defecto' }) => {
     return (
         <>
             <h2>{greet(greetingObject)}</h2>
             <h3>Valle López</h3>
+            <h4>{title}</h4>
         </>
     );
 }
