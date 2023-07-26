5783c93b986815b2c8c25018fb7236142654385e
lautarovallelopez <lautarovallelopez@gmail.com>
2023-07-25 23:00:27 -0300

    Create new component with my name

---
+++ 01-counter-app-cra/src/FirstApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FirstApp = () => {
+    return <h2>Lautaro</h2>;
+}
+
+export default FirstApp;
--- 01-counter-app-cra/src/index.jsx
+++ 01-counter-app-cra/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import FirstApp from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <FirstApp />
     </React.StrictMode>
 );
