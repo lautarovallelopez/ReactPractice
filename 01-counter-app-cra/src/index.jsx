5237c5e73ddbc8d9e0b30f4b170e8a85621a4dff
lautarovallelopez <lautarovallelopez@gmail.com>
2023-07-26 14:45:11 -0300

    Add default properperties

--- 01-counter-app-cra/src/index.jsx
+++ 01-counter-app-cra/src/index.jsx
@@ -6,6 +6,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title='Estoy practicando!!!' />
+        <FirstApp />
     </React.StrictMode>
 );
