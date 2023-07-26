5a6f1eae615490a116977762f676835bba8b0de3
lautarovallelopez <lautarovallelopez@gmail.com>
2023-07-25 23:33:35 -0300

    Add variable printing

--- 01-counter-app-cra/src/FirstApp.jsx
+++ 01-counter-app-cra/src/FirstApp.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const greetingObject = {
+    name: 'lautaro',
+    greeting: 'bienvenido'
+}
+const greet = greeting => `${greeting.greeting} ${greeting.name}`;
+
 const FirstApp = () => {
     return (
         <>
-            <h2>Lautaro</h2>
+            <h2>{greet(greetingObject)}</h2>
             <h3>Valle López</h3>
         </>
     );
