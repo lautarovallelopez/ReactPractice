d845582f82cdce1cd45eae4601cd0e75ff21dee3
lautarovallelopez <lautarovallelopez@gmail.com>
2023-07-28 19:25:30 -0300

    Add hook function to change counter value

--- 01-counter-app-cra/src/CounterApp.jsx
+++ 01-counter-app-cra/src/CounterApp.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const CounterApp = ({ value }) => {
+    const [counter, setCounter] = useState(value);
     const handleAdd = () => {
-        value = value + 1;
+        setCounter(currentCounter => currentCounter + 1);
     }
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={handleAdd}>+1</button>
         </>
     );
