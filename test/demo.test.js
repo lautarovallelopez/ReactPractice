ae875c8243839bc05d31f319c1fdd1a74a065c45
lautarovallelopez <lautarovallelopez@gmail.com>
2023-07-29 00:24:11 -0300

    Use test and describe functions examples

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,3 +1,12 @@
-test('Esta prueba no debe fallaar', () => {
-    throw new Error('asdasd');
+describe('Pruebas demo component', () => {
+    test('Mensaje 1 es igual a mensaje 2', () => {
+        // 1. Inicialización
+        const message1 = 'Hola mundo';
+    
+        // 2. Estímulo
+        const message2 = message1.trim();
+    
+        // 3. Observar el comportamiento esperado
+        expect(message1).toBe(message2);
+    });
 });
