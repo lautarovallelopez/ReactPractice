a53bab6874d1198e8bae4d053471554cf8f24ab5
lautarovallelopez <lautarovallelopez@gmail.com>
2023-07-29 01:22:24 -0300

    Add array test to 07-deses-arr

---
+++ test/test-bases/07-deses-arr.test.js
@@ -0,0 +1,18 @@
+import { returnArray } from "../../src/test-bases/07-deses-arr";
+
+describe('Pruebas en 07-deses-arr', () => {
+    test('Debe retornar un string y un número', () => {
+        const [letters, numbers] = returnArray();
+
+        // espera que sean los mismos valores
+        expect(letters).toBe('ABC');
+        expect(numbers).toBe(123);
+
+        // espera que sean del mismo tipo
+        expect(typeof letters).toBe('string');
+        expect(typeof numbers).toBe('number');
+
+        // espera que sea del msmo tipo
+        expect(letters).toEqual(expect.any(String));
+    });
+});
