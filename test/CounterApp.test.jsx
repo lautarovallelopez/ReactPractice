435b69eb1448a57ae2eb92b40aa47452c4cc1ccf
lautarovallelopez <lautarovallelopez@gmail.com>
2023-08-09 17:33:16 -0300

    Test Counter App on click event

--- test/CounterApp.test.jsx
+++ test/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CounterApp from "../src/CounterApp";
 
 const value = 100;
@@ -9,6 +9,24 @@ describe('Prueba Counter App', () => {
     });
     test('Debe de mostrar el valor inicial de 100', () => {
         render(<CounterApp value={value} />);
+        expect(screen.getByText(value)).toBeTruthy();
+    });
+    test('Debe incrementar con el botón +1', () => {
+        render(<CounterApp value={value} />);
+        fireEvent.click(screen.getByText('+1'));
+        expect(screen.getByText(value + 1)).toBeTruthy();
+    });
+    test('Debe decrementar con el botón -1', () => {
+        render(<CounterApp value={value} />);
+        fireEvent.click(screen.getByText('-1'));
+        expect(screen.getByText(value - 1)).toBeTruthy();
+    });
+    test('Debe funcionar el boton de reset', () => {
+        render(<CounterApp value={value} />);
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByText('+1'));
+        fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
         expect(screen.getByText(value)).toBeTruthy();
     });
 });
